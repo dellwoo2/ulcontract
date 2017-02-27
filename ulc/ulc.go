739f130925c44eb31ecbb3ea0860f915c8b07cbf
Danny Ellwood <dellwoo2@asiapac.globalcsc.net>
2017-02-27 12:20:17 +0800

    dffdfdf

--- ulc/ulc.go
+++ ulc/ulc.go
@@ -77,8 +77,8 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	if len(args) != 8 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 8")
 	}
         count=0;
  	//xx = &shared.Args{1, 2} 
